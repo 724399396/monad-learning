3eace5109691e8028aeca757e8ea4ca38c0b64a2
liww.li <liww.li@qunar.com>
2017-04-01 17:48:50 +0800

    finish 22.1

--- example22.hs
+++ example22.hs
@@ -14,4 +14,33 @@ logMsg s = do t <- liftIO getClockTime
 
 -- this handles one package
 filterOne :: [Rule] -> Packet -> LogWriter (Maybe Packet)
+filterOne rules packet = do rule <- return (match rules packet)
+                            case rule of
+                              Noting -> do logMsg ("DROPPING UNMATCHED PACKET: " ++ (show packet)
+                              (Just r) -> do when (logIt r) (logMsg ("MATCH: " ++ (show r) ++ " <=> " ++ (show packet)))
+                                             case r of
+                                               (Rule Accept _ _ ) -> return (Just packet)
+                                               (Rule Reject _ _ ) -> return Nothing
 
+-- this filters a list of packets, producing a filtered packet list
+-- and a log of the activity
+filterAll :: [Rule] -> [Packet] -> LogWriter [Packet]
+filterAll rules packets = do logMsg "STARTING PACKET FILTER"
+                             out <- mapM (filterOne rules) packets
+                             logMsg "STOPPING PACKET FILTER"
+                             return (catMaybes out)
+
+-- read the rule data from the file named in the first argument, and the packet data from
+-- the file named in the second argument, and the print the accepted packets followed by
+-- a log generated during the computation.
+main :: IO ()
+main = do args       <- getArgs
+          ruleData   <- readFile (args!!0)
+          packetData <- readFile (args!!1)
+          let rules   = (read ruleData)::[Rule]
+              packets = (read packetData)::[Packet]
+          (out,log) <- runWriteT (filterAll rules packets)
+          putStrLn "ACCEPTED PACKETS"
+          putStr (unlines (map show out))
+          putStrLn "\n\nFIREWALL LOG"
+          putStr (unlines (map show log))
